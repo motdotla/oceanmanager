0c296097fb59e43eebfd71706c70a666071ef09b
scottmotte <scott@scottmotte.com>
2013-06-17 15:46:45 +0000

    Attempt for heroku logging

--- clock.rb
+++ clock.rb
@@ -2,10 +2,16 @@ require File.dirname(__FILE__) + '/config/boot.rb'
 
 include Clockwork
 
+every(1.minute, 'text') do
+  puts "hi there!"
+  puts "="*100
+end
+
 every(2.minutes, 'document.done check') do
   time_ago  = (Time.now.utc-5.minutes).to_i * 1000 
   events    = Carve::Event.all({type: "document.done"}).events
   events.each do |event|
+    puts event.id
     if event.created > time_ago
       puts "="*100
       puts "Event was created: #{event.id}"
