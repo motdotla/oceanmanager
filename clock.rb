b84a276406e0e42d07bce69e8f3581d259501ff8
scottmotte <scott@scottmotte.com>
2013-06-17 15:49:16 +0000

    Working

--- clock.rb
+++ clock.rb
@@ -2,17 +2,11 @@ require File.dirname(__FILE__) + '/config/boot.rb'
 
 include Clockwork
 
-every(1.minute, 'text') do
-  puts "hi there!"
-  puts "="*100
-end
-
 every(2.minutes, 'document.done check') do
-  time_ago  = (Time.now.utc-5.minutes).to_i * 1000 
+  time_ago  = (Time.now.utc-2.minutes).to_i * 1000 
   events    = Carve::Event.all({type: "document.done"}).events
   events.each do |event|
-    puts event.id
-    if event.created > time_ago
+    if event.created >= time_ago
       puts "="*100
       puts "Event was created: #{event.id}"
       puts "  - send email here"
